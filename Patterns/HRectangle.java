1b2cb508b776433f14b6359e7be548052700a474
Pousali Chowdhury <136922307+PousaliChowdhury@users.noreply.github.com>
2024-04-01 17:20:02 +0530

    Update HRectangle.java

--- Patterns/HRectangle.java
+++ Patterns/HRectangle.java
@@ -1,7 +1,7 @@
 package Patterns;
 import java.util.Scanner;
 
-//hollow rectangel 
+//hollow rectangle 
 public class HRectangle {
      public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
